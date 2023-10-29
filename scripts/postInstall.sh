6bef83032c7ac4600f5e3ab6ca91c627fa6a708c
AmsellemJoseph <amsellem.joseph@gmail.com>
2023-10-29 16:11:31 +0200

    fix

--- scripts/postInstall.sh
+++ scripts/postInstall.sh
@@ -5,3 +5,14 @@ set -o allexport; source .env; set +o allexport;
 echo "Waiting for software to be ready ..."
 sleep 30s;
 
+target=$(docker-compose port mirotalkwebrtc 9000)
+
+curl http://${target}/api/v1/user/login \
+  -H 'accept: application/json, text/plain, */*' \
+  -H 'accept-language: fr-FR,fr;q=0.9,en-US;q=0.8,en;q=0.7,he;q=0.6' \
+  -H 'cache-control: no-cache' \
+  -H 'content-type: application/json' \
+  -H 'pragma: no-cache' \
+  -H 'user-agent: Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/118.0.0.0 Safari/537.36' \
+  --data-raw '{"username":"'${ADMIN_USERNAME}'","email":"'${ADMIN_EMAIL}'","password":"'${ADMIN_PASSWORD}'"}' \
+  --compressed
--- scripts/preInstall.sh
+++ scripts/preInstall.sh
@@ -1,3 +1,34 @@
 #set env vars
 set -o allexport; source .env; set +o allexport;
 
+cat /opt/elestio/startPostfix.sh > post.txt
+filename="./post.txt"
+
+SMTP_LOGIN=""
+SMTP_PASSWORD=""
+
+# Read the file line by line
+while IFS= read -r line; do
+  # Extract the values after the flags (-e)
+  values=$(echo "$line" | grep -o '\-e [^ ]*' | sed 's/-e //')
+
+  # Loop through each value and store in respective variables
+  while IFS= read -r value; do
+    if [[ $value == RELAYHOST_USERNAME=* ]]; then
+      SMTP_LOGIN=${value#*=}
+    elif [[ $value == RELAYHOST_PASSWORD=* ]]; then
+      SMTP_PASSWORD=${value#*=}
+    fi
+  done <<< "$values"
+
+done < "$filename"
+
+cat << EOT >> ./.env
+
+EMAIL_HOST=tuesday.mxrouting.net
+EMAIL_PORT=25
+EMAIL_USERNAME=${SMTP_LOGIN}
+EMAIL_PASSWORD=${SMTP_PASSWORD}
+EOT
+
+rm post.txt
